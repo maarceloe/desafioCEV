ddf94f7d536fbbeb9852468b5eed32e2221b615c
Maarceloe <marceloverardino@hotmail.com>
2025-11-06 18:07:05 -0300

    challenges

---
+++ antecessor/calcular.php
@@ -0,0 +1,7 @@
+ <?php 
+    $numero = $_POST['numero'];
+    $antecessor = $numero - 1;
+    $sucessor = $numero + 1;
+
+    echo "O seu numero é $numero. O antecessor é $antecessor e o sucessor é $sucessor";
+?>
---
+++ antecessor/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor</title>
+</head>
+
+<body>
+    <h1>
+        VAMOS DESCOBRIR O ANTECESSOR E O SUCESSOR DO SEU NUMERO
+    </h1>
+    <form action="calcular.php" method="post">
+        <label for="number">Insira um numero..:</label>
+        <input type="number" name="numero" id="idnumero">
+        <input type="submit" value="calcular">
+    </form>
+</body>
+
+</html>
