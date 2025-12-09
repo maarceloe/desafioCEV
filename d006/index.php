c0df0e0613a3f0e3008dc1d40ddf003842b6e743
Marcelinho <marceloverardino@hotmail.com>
2025-12-08 21:34:57 -0300

    i dont remember

--- d006/index.php
+++ d006/index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 006</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    
+    <main>
+        <h1>Anatomia de uma divisão</h1>
+        <form action="<?=  $_SERVER['PHP_SELF'] ?>"></form>
+    </main>
 </body>
 </html>
